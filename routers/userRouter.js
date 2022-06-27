ee92964933fbc6b551741d8d17f322df3cef2eb8
nastassia <anastasiyamihalenko@gmail.com>
2022-06-27 12:45:43 +0300

    server auth2.0

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -4,7 +4,6 @@ import {registerValidation} from '../middleware/authValidaions.js';
 import checkAuth from '../middleware/checkAuth.js';
 
 const router = new Router();
-
 router.post('/login', UserController.login);
 router.post('/register', registerValidation, UserController.register);
 router.get('/me', checkAuth, UserController.getMe);
